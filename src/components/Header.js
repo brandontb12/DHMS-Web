d457e400e460803a57466e4c3d5725b8182eceb9
Whit Matthews <whitmatthews@msn.com>
2019-11-20 18:46:23 -0500

    Working on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import logo from "../assets/images/dhms-logo.png"
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></Link>
+        <img src={logo}></img>
+        <Link to="/" className="logo"><span class="header">DEFENSE HEALTH MANAGEMENT SYSTEMS</span>&nbsp;<span class="subheader">Program Executive Office</span></Link>
         <nav>
             <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
         </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -25,8 +25,6 @@ class HomeIndex extends React.Component {
                 >
                 </Helmet>
 
-                <Banner />
-
                 <div id="main">
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
