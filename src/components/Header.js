85304100b0c8a45ad47888bf85cab67b7e8efd66
Schmidtmann, Scott <sschmidtmann@FTI-NET.COM>
2019-12-31 08:30:55 -0500

    Changed the logo within the header to utilize gatsby-image fized.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
-import { Navbar, Nav, NavItem, MenuItem} from 'react-bootstrap'
-import logo from "../assets/images/dhms-logo.png"
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Navbar, Nav, NavItem, MenuItem } from 'react-bootstrap';
+import { graphql, StaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
-const Header = (props) => (
-    // <header id="header" className="alt">
-    //     <img src={logo}></img>
-    //     <Link to="/" className="logo"><span class="header">DEFENSE HEALTH MANAGEMENT SYSTEMS</span>&nbsp;<span class="subheader">Program Executive Office</span></Link>
-    //     <nav>
-    //         <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
-    //     </nav>
-    //     <ul>
-    //         <li><Link to="/">Home</Link></li>
-    //         <li><Link to="/landing">Landing</Link></li>
-    //         <li><Link to="/generic">Generic</Link></li>
-    //         <li><Link to="/elements">Elements</Link></li>
-    //     </ul>
-    // </header>
-    <>
-    <Navbar id="header" className="alt">
-        <img className="pic-attributes" src={logo}></img>
-        <Nav.Link target="_top" href="/" className="logo"><span class="header">DEFENSE HEALTH MANAGEMENT SYSTEMS</span>&nbsp;<span class="subheader">Program Executive Office</span></Nav.Link>
-    </Navbar>
-    <Navbar className="nav-bar">
-        <Nav.Link target="_top" className="nav-tabs" href="/">Home</Nav.Link>
-        <Nav.Link target="_top" className="nav-tabs" href="/landing">Landing</Nav.Link>
-        <Nav.Link target="_top" className="nav-tabs" href="/generic">Generic</Nav.Link>
-        <Nav.Link target="_top" className="nav-tabs" href="/elements">Elements</Nav.Link>
-    </Navbar>
-    </>
-)
+const Header = ({ props }) => {
+    return (
+        <StaticQuery
+            query={query}
+            render={data => (
+                <>
+                    <Fragment>
+                        <Navbar id="header" className="alt">
+                            <Img className="pic-attributes" fixed={data.file.childImageSharp.fixed} />
+                            <Nav.Link target="_top" href="/" className="logo"><span class="header">DEFENSE HEALTH MANAGEMENT SYSTEMS</span>&nbsp;<span class="subheader">Program Executive Office</span></Nav.Link>
+                        </Navbar>
+                        <Navbar className="nav-bar">
+                            <Nav.Link target="_top" className="nav-tabs" href="/">Home</Nav.Link>
+                            <Nav.Link target="_top" className="nav-tabs" href="/landing">Landing</Nav.Link>
+                            <Nav.Link target="_top" className="nav-tabs" href="/generic">Generic</Nav.Link>
+                            <Nav.Link target="_top" className="nav-tabs" href="/elements">Elements</Nav.Link>
+                        </Navbar>
+                    </Fragment>
+                </>
+            )}
+        />
+    );
+};
+
+export const query = graphql`
+    query {
+        file(relativePath: { eq: "dhms-logo.png" }) {
+            childImageSharp {
+                fixed {
+                    ...GatsbyImageSharpFixed
+                }
+            }
+        }
+    }
+`;
 
 Header.propTypes = {
     onToggleMenu: PropTypes.func
-}
+};
 
-export default Header
+export default Header;
